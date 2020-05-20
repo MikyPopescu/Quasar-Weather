15f605834bc7ee80aac93fba8ae3e2f55fa1fc98
MikyPopescu <popescu9andreea17@stud.ase.ro>
2020-05-20 20:29:47 +0300

    weather by location

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,99 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input v-model="search" @keyup.enter="getWeatherBySearch" label="search" dark borderless>
+        <template v-slot:before>
+          <q-icon @click="getLocation" name="my_location"></q-icon>
+        </template>
+
+        <template v-slot:append>
+          <q-btn @click="getWeatherBySearch" round dense flat icon="search"></q-btn>
+        </template>
+      </q-input>
+    </div>
+
+    <template v-if="weatherData">
+      <div class="col text-white text-center">
+        <div class="text-h4 text-weight-light">{{weatherData.name}}</div>
+        <div class="text-h6 text-weight-white">{{weatherData.weather[0].name}}</div>
+        <div class="text-h1 text-weight-thin q-my-lg relative-position">
+          <span>{{Math.round(weatherData.main.temp)}}</span>
+          <span class="text-h4 relative-position degree">&deg;C</span>
+        </div>
+      </div>
+
+      <div class="col text-center">
+        <img :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" />
+      </div>
+    </template>
+
+    <template v-else>
+      <div class="col column text-center text-white">
+        <div class="col text-h2 text-weight-thin">
+          Quasar
+          <br />Weather
+        </div>
+        <q-btn @click="getLocation" class="col" flat>
+          <q-icon left size="3em" name="my_location" />
+          <div>Find my Location</div>
+        </q-btn>
+      </div>
+    </template>
+
+    <div class="col skyline"></div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      search: '',
+      weatherData: null,
+      lat: null,
+      lon: null,
+      apiURL: `https://api.openweathermap.org/data/2.5/weather`,
+      apiKey: `` ///https://openweathermap.org/weather-conditions free account for apiKey
+    }
+  },
+  methods: {
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(position => {
+        // console.log(position)
+        this.lat = position.coords.latitude
+        this.lon = position.coords.longitude
+        this.getWeatherByCoords()
+      })
+    },
+    getWeatherByCoords() {
+      this.$axios(
+        `${this.apiURL}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
+      ).then(response => {
+        // console.log(response)
+        this.weatherData = response.data
+      })
+    },
+    getWeatherBySearch() {
+      this.$axios(
+        `${this.apiURL}?q=${this.search}&appid=${this.apiKey}&units=metric`
+      ).then(response => {
+        this.weatherData = response.data
+      })
+    }
+  }
 }
 </script>
+
+
+<style lang="sass">
+.q-page
+  background: linear-gradient(to bottom,#136a8a,#267871)
+.degree
+  top: -44px
+.skyline
+  flex: 0 0  100px
+  background: url('../statics/skyline.png')
+  background-size: contain
+  background-position: center bottom
+</style>
